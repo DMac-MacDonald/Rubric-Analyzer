70f132723f93321d1449dd45c11daac57056e1c9
Jane MacDonald <112093094+DMac-MacDonald@users.noreply.github.com>
2025-12-01 08:12:42 -0500

    Comments

--- rubric_analyzer_2.py
+++ rubric_analyzer_2.py
@@ -1,31 +1,44 @@
+#csv: used to write and read from csv files
+#os: used for batch processing
+#PIL: main library, used for image processing and pixel detection
 import csv
 import os
-from PIL import Image
+from PIL import Image 
 
-cardDirectory = "Cards"
+
+
+#lineBox[]: hard coded list of 4-tuple box locations/sizes for each line of bubbles (will be dynamic later)
+#bubbleBox[]: hard coded list of 4-tuple box locations/sizes for each bubble on a line (will be dynamic later)
+#scoreList[]: list to be written to the .csv file at the end of the program
+#cardDirectory: string local folder name for card images
+#imageSize: tuple variable for setting the image size for reliability 
+#scoreDetect: bool variable for checking if a score has been found
+cardDirectory = "Cards" 
 lineBox = [(225,50,499,80),(225,90,499,120),(225,130,499,160),(225,170,499,200),(225,210,499,240),(225,255,499,295),(225,295,499,335),(225,335,499,375),(225,380,499,416),(225,415,499,456),(225,460,499,490)]
 bubbleBox = [(16,5,40,30),(70,5,95,30),(124,5,149,30),(175,5,210,30),(235,5,260,30)]
-scoreList = []
+scoreList = [] 
 imageSize = (500,500)
 scoreDetect = 0
 print("running....")#start of program
 
 
-for idxF, filename in enumerate(os.listdir(cardDirectory)):
+for idxF, filename in enumerate(os.listdir(cardDirectory)): #Loop to allow for checking every card image in the cards folder
     scoreIndvList = []
     file_path = os.path.join(cardDirectory, filename)
     infile = file_path
     image = Image.open(infile)
     image = image.resize(imageSize)
-    image = image.convert("RGB")
 
-    for idxJ, j in enumerate(lineBox):
+    image = image.convert("L") #convert image to greyscale (to avoid off colors from irl pictures)
+    image.save("output1.jpg") #testing image
+    image = image.convert("RGB")#converts image to rgb image for easy color detection, retains greyscale change
+
+    for idxJ, j in enumerate(lineBox):#Line for loop
 
         currentLine = image.crop(j)#crops the image to each line
 
-        for idxI, i in enumerate(bubbleBox):
-            if idxJ == 8:
-                currentLine.save("output1.jpg")
+        for idxI, i in enumerate(bubbleBox):#Bubble for loop
+
             scoreDetect = 0 #sets detect variable to false
             currentBubble = currentLine.crop(i) #crops line iamge to each bubble
 
@@ -37,12 +50,15 @@ for idxF, filename in enumerate(os.listdir(cardDirectory)):
                     
             if scoreDetect == 1: #If black was detected in the bubble image
                 print("Bubble:",idxI+1, ", Line:", idxJ+1)
-                if idxI == 0:#write scores to the txt file
+                if idxI == 0:#appends scores to individual list if detected
                     scoreIndvList.append(str(idxI))
                 else:
                     scoreIndvList.append(str(idxI+1))
-    scoreList.append(scoreIndvList)
-    with open('scores.csv', 'w', newline='') as csvfile:
+
+
+    scoreList.append(scoreIndvList)#appends the batch score list with the individual list of scores
+
+    with open('scores.csv', 'w', newline='') as csvfile: #updates .csv file with collected scores
         writer = csv.writer(csvfile)
         writer.writerows(scoreList)
 
